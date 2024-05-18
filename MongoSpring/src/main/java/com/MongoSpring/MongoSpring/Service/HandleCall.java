ec51e84fa2dec76ab25d4205e1628e9689a46eb6
Rishi Sivaraju <sivarajurishi@gmail.com>
2024-05-18 21:04:08 +0530

    MCQ done

--- MongoSpring/src/main/java/com/MongoSpring/MongoSpring/Service/HandleCall.java
+++ MongoSpring/src/main/java/com/MongoSpring/MongoSpring/Service/HandleCall.java
@@ -1,5 +1,4 @@
 package com.MongoSpring.MongoSpring.Service;
-
 import com.MongoSpring.MongoSpring.Model.AdminDetails;
 import com.MongoSpring.MongoSpring.Model.McqCredentails;
 import com.MongoSpring.MongoSpring.Model.ProfileDetails;
@@ -9,65 +8,6 @@ import com.MongoSpring.MongoSpring.Repository.MCQRepo;
 import com.MongoSpring.MongoSpring.Repository.UserRepo;
 import com.MongoSpring.MongoSpring.Repository.ProfileRepo;
 import org.springframework.stereotype.Service;
-
-// @Service
-// public class HandleCall {
-//     private final UserRepo repository;
-//     private final ProfileRepo repository1;
-
-//     private final AdminRepo repository2;
-
-//     private final MCQRepo repository3;
-
-//     public HandleCall(UserRepo repository, ProfileRepo repository1, AdminRepo repository2, MCQRepo repository3) {
-//         this.repository = repository;
-//         this.repository1 = repository1;
-//         this.repository2 = repository2;
-//         this.repository3 = repository3;
-//     }
-
-//     public UserDetails getUserDetails(String user) {
-//         UserDetails Requser = repository.findByusername(user);
-//         return Requser;
-//     }
-
-//     public UserDetails getUserDetailsbyid(String user) {
-//         UserDetails Requser = repository.findByid(user);
-//         return Requser;
-//     }
-
-//     public void postUserDetails(UserDetails userDetails) {
-//         repository.insert(userDetails);
-
-//     }
-
-//     public void postProfileDetails(ProfileDetails profileDetails) {
-//         repository1.insert(profileDetails);
-//     }
-
-//     public ProfileDetails getProfileDetails(long user) {
-//         ProfileDetails requser = repository1.findBymobile(user);
-//         return requser;
-//     }
-
-//     public void postAdminDetails(AdminDetails adminDetails) {
-//         repository2.insert(adminDetails);
-//     }
-
-//     public AdminDetails getAdminDetails(String username) {
-//         AdminDetails reqAdmin = repository2.findByusername(username);
-//         return reqAdmin;
-//     }
-
-//     public McqCredentails getMcqCredentails(String question) {
-//         McqCredentails reqquestion = repository3.findByquestion(question);
-//         return reqquestion;
-//     }
-
-//     public void postMCQcredentials(McqCredentails mcqCredentails) {
-//         repository3.insert(mcqCredentails);
-//     }
-// }
 import java.util.List;
 
 @Service
